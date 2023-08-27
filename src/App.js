c00390d062253d4b53b7810222fb4d73c117d950
Joao-an <82965788+Joao-an@users.noreply.github.com>
2023-08-26 22:09:00 -0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,25 +1,57 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      email: "",
+      senha: "",
+      mensagem: "",
+    };
+  }
+
+  handleEmailChange = (event) => {
+    this.setState({ email: event.target.value });
+  };
+
+  handleSenhaChange = (event) => {
+    this.setState({ senha: event.target.value });
+  };
+
+  handleAcessar = () => {
+    const { email, senha } = this.state;
+    if (email === "eduardo.lino@pucpr.br" && senha === "123456") {
+      this.setState({ mensagem: "Acessado com sucesso!" });
+    } else {
+      this.setState({ mensagem: "Usuário ou senha incorretos!" });
+    }
+  };
+
+  render() {
+    return (
+      <div className="App">
+        <h1>Login</h1>
+        <div>
+          <input
+            type="email"
+            placeholder="Email"
+            value={this.state.email}
+            onChange={this.handleEmailChange}
+          />
+        </div>
+        <div>
+          <input
+            type="password"
+            placeholder="Senha"
+            value={this.state.senha}
+            onChange={this.handleSenhaChange}
+          />
+        </div>
+        <button onClick={this.handleAcessar}>Acessar</button>
+        <p>{this.state.mensagem}</p>
+      </div>
+    );
+  }
 }
 
 export default App;
